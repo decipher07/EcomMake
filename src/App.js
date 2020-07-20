4231b8cd14a1a1c683bd7192f9e7f645fd4009a8
DJ <social.deej@gmail.com>
2020-07-20 15:59:54 +0530

    Setting Up Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
@@ -16,7 +15,7 @@ class App extends Component{
     fetch(`https://jsonplaceholder.typicode.com/users`)
     .then((res) => res.json())
     .then((users) => {
-      return this.setState({ monsters: users})
+      this.setState({ monsters: users})
     })
   }
 
