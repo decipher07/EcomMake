282dedfe32b74b9d7e9bb07864ba50225ca5e53a
DJ <social.deej@gmail.com>
2020-07-23 15:45:01 +0530

    Changing Schema Structure

---
+++ ecommake/ecommake/src/components/directory/directory.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component'
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections:[
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1 
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1 
+                },
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1 
+                },
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1 
+                },
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1 
+                },
+            ]
+        }
+    }
+}
