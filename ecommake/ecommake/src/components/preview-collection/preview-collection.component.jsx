dd4e3603b00f49d287791035919e927b100630fe
DJ <social.deej@gmail.com>
2020-07-25 09:32:31 +0530

    Defining A Schema

---
+++ ecommake/ecommake/src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './preview-collection.styles.scss'
+
+const previewCollection = ({title}) => (
+    <div className='collection-preview'>
+        <h1 className='title'> {title.toUpperCase()} </h1>
+        <div className='preview'>
+
+        </div>
+    </div>
+)
