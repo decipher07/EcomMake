890e080e99af5bc07263595834619b280994e694
DJ <social.deej@gmail.com>
2020-07-25 09:38:09 +0530

    Changing the Location of lInking

--- ecommake/ecommake/src/App.js
+++ ecommake/ecommake/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
-
+ 
 function App() {
   return (
     <div> 
--- ecommake/ecommake/src/pages/shop/shop.component.jsx
+++ ecommake/ecommake/src/pages/shop/shop.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Shop_Data from './shop.data'
 
+import PreviewCollection from '../components/preview-collection/preview-collection.component'
+
+
 class ShopPage extends React.Component {
     constructor(props){
         super();
