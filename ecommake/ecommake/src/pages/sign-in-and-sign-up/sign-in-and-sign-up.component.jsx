a6032663c0bb35aaf62cf5d4b2cba37ea2101b6b
DJ <social.deej@gmail.com>
2020-07-25 22:20:25 +0530

    Adding Context

---
+++ ecommake/ecommake/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.component';
+
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        SIGN IN
+    </div>
+)
+
+
+export default SignInAndSignUpPage;
